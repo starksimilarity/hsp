efe1d1dc7eee196262f4fe15baf3ad05ea671592
starksimilarity <starksimilarity@gmail.com>
2019-11-10 21:55:55 -0600

    adds setup.py for install

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+from os import path
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="high-speed-playback",
+    author="starksimilarity",
+    version="0.1a",
+    author_email="starksimilarity@gmail.com",
+    description="A Python library to replay command line sessions",
+    license="GPL",
+    packages=find_packages(),
+    install_requires=["prompt_toolkit>=2.0", "setuptools"],
+    python_requires=">=3.6.0",
+    url="https://github.com/starksimilarity/hsp",
+    download_url="https://github.com/starksimilarity/hsp/archive/master.zip",
+    long_description=read("README.md"),
+)
